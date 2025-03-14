bf806b40a8e50503edd6f69873d84f0fcb131849
unknown <vinicius.oliveira@grupotechnik.com.br>
2025-03-14 07:47:02 -0300

    Deploy sources

--- teste.py
+++
@@ -1,7 +0,0 @@
-import pyautocad
-
-acad = pyautocad.Autocad(create_if_not_exists=True)
-acad.app.Documents.Open(r"c:\Users\Vinicius\Technik\Clientes - Documentos\AI\AutoCAD\Alltech\Automação de Desenho fase 1\draws\Circulo.dwg")
-
-for obj in acad.iter_objects():
-    print(obj)
